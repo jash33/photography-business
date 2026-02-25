6ce70e7af9becb21c655b5c9afa701a4cbf12402
Ubuntu <ubuntu@ip-172-31-21-231.ec2.internal>
2026-02-25 08:02:10 +0000

    Fix base URL trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jash33.github.io',
-  base: '/photography-business',
+  base: '/photography-business/',
 });
